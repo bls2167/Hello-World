da8fc5e5410b8e05b32a410abf2378e26d396b57
Ben Solomon <bls2167@columbia.edu>
2015-01-22 14:09:55 -0500

    Initial commit

---
+++ src/example/helloworld/HelloWorld.java
@@ -0,0 +1,10 @@
+package example.helloworld;
+
+/**
+ * Created by bensolomon on 1/20/15.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
