eed46d978f693aa31f6627f48262d7ad583a2044
bls2167 <bls2167@columbia.edu>
2015-01-22 14:11:37 -0500

    Changed output

--- src/example/helloworld/HelloWorld.java
+++ src/example/helloworld/HelloWorld.java
@@ -5,6 +5,6 @@ package example.helloworld;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World, you beautiful thing you!");
     }
 }
